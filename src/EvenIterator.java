f8ecb1c1dbebaa4113c514baf0a2acc0d11e90c0
Tarakaramk <tarakaram.koppada@netcracker.com>
2022-11-15 16:40:14 +0530

    seven tasks

--- src/EvenIterator.java
+++ src/EvenIterator.java
@@ -1,11 +1,16 @@
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class EvenIterator {
     public void numberIterator() {
-        int[] numbers = {12, 8, 2, 23, 4, 1, 3, 5, 6, 7};
+        Integer[] numbers = {12, 8, 2, 23, 4, 1, 3, 5, 6, 7};
+        Iterator<Integer> iterator = Arrays.asList(numbers).iterator();
 
-
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] % 2 == 0) {
-                System.out.println(numbers[i]);
+        int newnum=0;
+        while(iterator.hasNext()) {
+            newnum=iterator.next();
+            if (newnum % 2 == 0) {
+                System.out.println(newnum);
             }
         }
     }
